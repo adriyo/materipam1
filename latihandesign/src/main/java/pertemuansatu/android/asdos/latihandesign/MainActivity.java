67355ac6c836aff99fa0f588733fd8f87b17bcbf
adriyo <adriyoutomo@gmail.com>
2015-07-01 00:00:31 +0700

    first commit

--- latihandesign/src/main/java/pertemuansatu/android/asdos/latihandesign/MainActivity.java
+++ latihandesign/src/main/java/pertemuansatu/android/asdos/latihandesign/MainActivity.java
@@ -1,17 +1,21 @@
 package pertemuansatu.android.asdos.latihandesign;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,16 @@ public class MainActivity extends ActionBarActivity {
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        imgView = (ImageView) findViewById(R.id.imageView);
+        imgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 
 
@@ -65,6 +79,19 @@ public class MainActivity extends ActionBarActivity {
         if(id == R.id.action_notification){
             startActivity(new Intent(MainActivity.this, NotificationActivity.class));
         }
+
+        if(id == R.id.action_sms){
+            startActivity(new Intent(MainActivity.this, SMSMainActivity.class));
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            imgView.setImageURI(imageUri);
+        }
     }
 }
