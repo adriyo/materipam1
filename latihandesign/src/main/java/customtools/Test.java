acde77d37bf147b8f5a6894471c9c8debefc96ec
unknown <febrianhendro.cs@gmail.com>
2017-01-10 00:19:54 +0700

    tambahan class Coba

---
+++ latihandesign/src/main/java/customtools/Test.java
@@ -0,0 +1,12 @@
+package customtools;
+
+/**
+ * Created by admin on 3/9/2015.
+ */
+public class DataMahasiswa {
+    public String nim;
+    public String nama;
+    public String fakultas;
+
+    
+}
